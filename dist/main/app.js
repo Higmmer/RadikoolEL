d15d54e1ada2c7124bcad54f5f30658beb8144d1
Higmmer <881172+Higmmer@users.noreply.github.com>
2021-02-24 04:30:48 +0900

    開発環境構築

--- dist/main/app.js
+++ dist/main/app.js
@@ -13,7 +13,13 @@ var mainWindow;
 
 function createWindow () {
     // Create the browser window.
-    mainWindow = new BrowserWindow({width: 800, height: 600})
+    mainWindow = new BrowserWindow({width: 800, height: 600,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+            enableRemoteModule: true
+        }
+    });
     //mainWindow.webContents.openDevTools();
     // and load the index.html of the app.
     mainWindow.loadURL(url.format({
